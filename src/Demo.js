9afd8a4d044f2203b71f62be8845127e7beed821
raphaelcanadazy <raphaelcanadazy@gmail.com>
2023-06-11 19:48:19 -0400

    first commit

---
+++ src/Demo.js
@@ -0,0 +1,12 @@
+import React from 'react';
+
+const Demo = () => {
+  return (
+    <div>
+      <h1>Hello, React Demo!</h1>
+      <p>This is a simple demo of a React.js application.</p>
+    </div>
+  );
+};
+
+export default Demo;
